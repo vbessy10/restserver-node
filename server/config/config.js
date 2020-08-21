9c2ac36456099e46c9856738bbb79d7306bd5806
Bessy <vbessy798@yahoo.es>
2020-08-21 00:30:45 -0600

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,23 @@
 //***************************/
 
 process.env.PORT = process.env.PORT || 3000;
+
+//***************************/
+/*          Entorno         */
+//***************************/
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//***************************/
+/*            DB            */
+//***************************/
+
+let urlDB;
+
+if (process.env.NODE_ENV == 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb+srv://Mariela10:jIdhx1VLPLytVgUq@cluster0.ktmt5.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
